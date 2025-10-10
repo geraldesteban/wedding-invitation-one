f453595c031130732abb47293e4858ea8895c186
geraldesteban <wakin.esteban@gmail.com>
2025-10-11 06:23:04 +0800

    chore: test

--- app/page.js
+++ app/page.js
@@ -5,7 +5,6 @@ import { useEffect, useRef, useState } from "react";
 import Logo from "@/app/_components/Logo";
 import Header from "./_components/Navigation/Header";
 import Home from "@/app/_components/Sections/Home";
-import LoveStory from "@/app/_components/Sections/LoveStory";
 import YouAreInvited from "@/app/_components/YouAreInvited";
 import Entourage from "@/app/_components/Sections/Entourage";
 import Timeline from "@/app/_components/Sections/Timeline";
@@ -15,6 +14,7 @@ import FAQS from "@/app/_components/Sections/FAQS";
 import Location from "@/app/_components/Sections/Location";
 import DaysLeft from "@/app/_components/DaysLeft";
 import RSVP from "@/app/_components/Sections/RSVP";
+import LoveStory from "@/app/_components/Sections/LoveStory";
 
 function Page() {
   const logoRef = useRef(null);
@@ -51,11 +51,6 @@ function Page() {
       <Entourage />
       <Timeline />
       <Attire />
-      <Details />
-      <FAQS />
-      <Location />
-      <DaysLeft />
-      <RSVP />
     </>
   );
 }
